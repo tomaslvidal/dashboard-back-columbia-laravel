b64cc47ebbd88d5a6652badb12717242a4960998
Vidal Tomás <vtcheqeeh@gmail.com>
2018-08-03 11:08:13 -0300

    Events
    solo user y voucher tienen codigo pero comentado por si a futuro lo uso, los demas no

---
+++ app/Events/UserRestored.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Columbia\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+use Carbon\Carbon;
+
+use Columbia\User;
+
+class UserRestored
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $user->vouchers()->sync(
+            array(
+                "1" => array( 'deleted_at' => NULL )
+            )
+        );
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
