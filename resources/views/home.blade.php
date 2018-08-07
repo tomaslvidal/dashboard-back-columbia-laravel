39d815841ea6b8d8f513ba865b52d4a22a4ac2fa
Vidal Tomás <vtcheqeeh@gmail.com>
2018-08-07 15:20:29 -0300

    Init Vue.js

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('template.layout')
+@section('title', 'Inicio')
+@section('content')
+  <example-component></example-component>
+@endsection
+@section('script')
+    <script>
+    </script>
+@endsection
