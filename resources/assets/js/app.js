95a1065a78b498adf5368a9932775fad21c7466f
Vidal Tomás <vtcheqeeh@gmail.com>
2018-08-13 09:54:15 -0300

    Configuration APP, add VUE/Vuex/Router/Components/Layouts/BootstrapVue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,36 @@
+import './bootstrap';
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import BootstrapVue from 'bootstrap-vue';
 
-require('./bootstrap');
+import VueRouter from 'vue-router';
+
+import Template from './Template.vue';
+
+import Default from './layouts/Default.vue';
+
+import Error404 from './layouts/Error404.vue';
+
+import router from './routes';
+
+import store from './store';
 
 window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.use(BootstrapVue);
+
+Vue.component('default-layout', Default);
+
+Vue.component('error404-layout', Error404);
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
+Vue.component('nav-component', require('./components/NavComponent.vue'));
+
+Vue.component('breadcrumb', require('./components/breadcrumb.vue'));
+
 const app = new Vue({
-    el: '.app'
-});
+    el: '.app',
+    store,
+    router,
+    render: h => h(Template)
+})
