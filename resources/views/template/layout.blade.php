d470f2dc15a3fb2607f82257e027596d778046c9
Vidal Tomás <vtcheqeeh@gmail.com>
2018-08-13 10:00:11 -0300

    Layout of blade (Laravel)

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -1,24 +1,41 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }} | @yield('title') </title>
-    <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('public/css/style.css') }}" rel="stylesheet">
-    <script>
+
+    <html lang="{{ app()->getLocale() }}">
+
+    <head>
+        <meta charset="utf-8">
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        {{-- <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
+
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }} | @yield('title') </title>
+
+        <link href="{{ asset('css/app.css?v='.filemtime(public_path('css/app.css')) ) }}" rel="stylesheet">
+
+        <link href="{{ asset('css/style.css?v='.filemtime(public_path('css/style.css')) ) }}" rel="stylesheet">
+
+        <script>
         window.Laravel = {!! json_encode(['csrfToken' => csrf_token()]) !!};
-    </script>
-    @yield('style')
-</head>
-<body>
-<div class="app">
-    @yield('content')
-</div>
-<script src="{{ asset('public/js/app.js') }}"></script>
-<script defer src="https://use.fontawesome.com/releases/v5.2.0/js/all.js" integrity="sha384-4oV5EgaV02iISL2ban6c/RmotsABqE4yZxZLcYMAdG7FAPsyHYAPpywE9PJo+Khy" crossorigin="anonymous"></script>
-@yield('script')
-</body>
+        </script>
+
+        @yield('style')
+    </head>
+
+    {{-- <body> --}}
+    <body>
+        <div class="app">            
+            @yield('content')
+        </div>
+
+        <script src="{{ asset('js/app.js?v='.filemtime(public_path('js/app.js'))) }}"></script>
+
+        <script defer src="https://use.fontawesome.com/releases/v5.2.0/js/all.js" integrity="sha384-4oV5EgaV02iISL2ban6c/RmotsABqE4yZxZLcYMAdG7FAPsyHYAPpywE9PJo+Khy" crossorigin="anonymous"></script>
+
+        @yield('script')
+    </body>
 </html>
