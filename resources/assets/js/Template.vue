94a165df388aece867cb52d4bedb10234562c7bf
Vidal Tomás <vtcheqeeh@gmail.com>
2018-08-13 10:26:31 -0300

    render views with layouts depending if exist the route

---
+++ resources/assets/js/Template.vue
@@ -0,0 +1,25 @@
+<template>
+    <component :is="layout">      
+      <router-view/>
+    </component>
+</template>
+
+<script>
+const default_layout = "default";
+
+export default {
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || default_layout) + "-layout";
+    }
+  },
+
+  // created() {
+  //   console.log(this.$route, this.$route.meta.layout);
+  // },
+
+  // updated() {
+  //   console.log(this.$route, this.$route.meta.layout);
+  // }
+};
+</script>
