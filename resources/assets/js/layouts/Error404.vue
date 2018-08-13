7061aa90f6f1df6e754d111e5dcf4e7115491d77
Vidal Tomás <vtcheqeeh@gmail.com>
2018-08-13 09:58:41 -0300

    Layouts, template(default.vue) and error(Error404.vue)

---
+++ resources/assets/js/layouts/Error404.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="errorVue">
+    <div class="column is-8">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ resources/assets/js/layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="loader">
+		<nav-component></nav-component>
+
+		<div class="content-wrapper">
+			<div class="container-fluid">
+				<breadcrumb></breadcrumb>
+
+				<div class="col-md-12">
+					<div class="row">
+						<slot/>
+					</div>
+				</div>
+
+				<footer class="sticky-footer">
+				  <div class="container">
+				    <div class="text-center">
+				      <small>Copyright © Columbria 2018</small>
+				    </div>
+				  </div>
+				</footer>
+			</div>
+		</div>
+
+		<b-modal ref="myModalRef" id="myModal" title="¿Listo para salir?" ok-title="Cerrar Sesión" cancel-title="Cancelar">
+			Seleccione "Cerrar sesión" a continuación si está listo para finalizar su sesión actual.
+		</b-modal>
+	</div>
+</template>
+
+<script>
+export default{
+	beforeCreate(){
+		let body = document.querySelector('body');
+
+		body.className += ' fixed-nav sticky-footer bg-dark';
+
+		body.setAttribute('id', 'page-top');
+	}
+}
+</script>
