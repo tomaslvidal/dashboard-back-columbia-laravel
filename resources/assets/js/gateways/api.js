8db3f1cfb69c0545c53eaa6d3e05b3f5df77dcaa
Vidal Tomás <vtcheqeeh@gmail.com>
2018-08-15 10:56:08 -0300

    API Gateways

---
+++ resources/assets/js/gateways/api.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+const client = axios.create({
+  baseURL: '/api',
+  json: true
+})
+
+export default{
+  async execute(method, resource, data){
+    // let accessToken = await Vue.prototype.$auth.getAccessToken()
+    return client({
+      method,
+      url: resource,
+      data,
+      headers: {
+        // Authorization: `Bearer ${accessToken}`
+      }
+    }).then(req => {
+      return req.data
+    })
+  },
+  get(model ,id){
+  	if(id!=undefined){
+    	return this.execute('get', `/${model}/${id}`)
+  	}
+  	else{
+    	return this.execute('get', `/${model}`)
+  	}
+  },
+  create(model, data){
+    return this.execute('post', `/${model}`, data)
+  },
+  update(model, id, data){
+    return this.execute('put', `/${model}/${id}`, data)
+  },
+  delete(model, id){
+    return this.execute('delete', `/${model}/${id}`)
+  }
+}
