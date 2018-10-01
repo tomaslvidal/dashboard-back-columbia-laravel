18666cb385f61be76d8cd2363bd2c35749a71d7a
Vidal Tomás <vtcheqeeh@gmail.com>
2018-10-01 09:25:21 -0300

    ADD Logout

--- resources/assets/js/components/ModalLogoutComponent.vue
+++ resources/assets/js/components/ModalLogoutComponent.vue
@@ -1,5 +1,5 @@
 <template>
-	<b-modal ref="myModalRef" @hide="toggleModal" :visible="showModal" id="myModal" title="¿Listo para salir?" ok-title="Cerrar Sesión" cancel-title="Cancelar">
+	<b-modal ref="myModalRef" @ok="okButton" @hide="toggleModal" :visible="showModal" id="myModal" title="¿Listo para salir?" ok-title="Cerrar Sesión" cancel-title="Cancelar">
 		Seleccione "Cerrar sesión" a continuación si está listo para finalizar su sesión actual.
 	</b-modal>
 </template>
@@ -14,7 +14,16 @@ export default{
   	methods:{
   		toggleModal(){
   			this.$store.dispatch('Modals/StateLogout');
-  		}
+  		},
+      okButton(){
+        delete axios.defaults.headers.common['Authorization'];
+
+        localStorage.removeItem('user-token');
+
+        this.$store.commit('Accounts/AUTHENTICATED_FALSE');
+
+        this.$router.push({name: 'index'});
+      }
   	}
 }
 </script>
