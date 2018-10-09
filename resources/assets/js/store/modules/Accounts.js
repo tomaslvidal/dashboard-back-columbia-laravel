390ef618298bf6320510d4f1a0ccca88a111e47e
Vidal Tomás <vtcheqeeh@gmail.com>
2018-10-09 09:26:04 -0300

    upgrade login

--- resources/assets/js/store/modules/Accounts.js
+++ resources/assets/js/store/modules/Accounts.js
@@ -1,5 +1,7 @@
 import createPersistedState from 'vuex-persistedstate'
 
+import router from '../../routes';
+
 const Accounts = {
     plugins: [createPersistedState()],
     namespaced: true,
@@ -12,21 +14,28 @@ const Accounts = {
         },
         currentUser: {
             name: null,
+            last_name: null,
             email: null,
+            id: null
         },
+        isAuthenticated: null,
     },
     mutations:{
-        SET_ITEMS(state, payload){
-            state.items = payload;
-
-            state.request_made = true;
+        UPDATE_TOKENS(state, payload){
+            state.tokens = payload;
+        },
+        AUTHENTICATED_TRUE(state, payload){
+            state.isAuthenticated = true;
+        },
+        AUTHENTICATED_FALSE(state, payload){
+            state.isAuthenticated = null;
+        },
+        SET_USER(state, payload){
+            state.currentUser = payload;
         }
-
     },
     actions:{
         async ENTER_ACCOUNT(context, user){
-                console.log('user', user)
-
                 let data = {
                     client_id: 2,
                     client_secret: 'YdDDU3QNKu290Uf4qoat5FQcBiseLXrI4fJD33aw',
@@ -36,14 +45,38 @@ const Accounts = {
                 };
 
                 axios.post('/oauth/token', data)
-                .then(response => {
-                    let responseData = response.data;
-                    let now = Date.now();
+                    .then(response => {
+                        let responseData = response.data;
+                        
+                        let now = Date.now();
 
-                    responseData.expires_in = responseData.expires_in + now;
+                        responseData.expires_in = responseData.expires_in + now;
 
-                    context.commit('updateTokens', responseData);
-                });
+                        context.commit('UPDATE_TOKENS', responseData);
+
+                        axios.defaults.headers.common['Authorization'] = 'Bearer '+context.state.tokens.access_token;
+
+                        localStorage.setItem('user-token', context.state.tokens.access_token);
+
+                        axios.get('/api/users/2')
+                            .then(response => {
+                                response = response.data[0];
+
+                                let data = {
+                                    "name" : response.name,
+                                    "last_name" : response.last_name,
+                                    "email" : response.email,
+                                    "id" : response.id
+                                }
+
+                                context.commit('SET_USER', data);
+
+                                context.commit('AUTHENTICATED_TRUE');
+
+                                router.push({ name: "home"})
+                            });
+                        
+                    });
         }
     }
 }
