0e44058413a5328bfca3f1ccbd4efd4e7029f739
Vidal Tomás <vtcheqeeh@gmail.com>
2018-10-09 09:26:31 -0300

    fix styles in body, then of logout

---
+++ resources/assets/js/layouts/Entry.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+<!--     <div class="d-flex justify-content-center">
+      <router-link :to="{ name: 'index' }"><img src="/img/logo.png" style="height: 200px; margin-left: -15px;"></router-link>
+    </div> -->
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  mounted(){
+    let body = document.querySelector('body'); 
+
+    if(body.classList){
+      body.removeAttribute("class");
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  color: #787878;
+  margin-top: 60px;
+  background-color: #FFF;
+}
+</style>
