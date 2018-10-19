8fb5d51dc02a2e1e31d6150eb086feebc39ee9ee
Vidal Tomás <vtcheqeeh@gmail.com>
2018-10-19 12:40:35 -0300

    upgrade navComponent

--- resources/assets/js/components/NavComponent.vue
+++ resources/assets/js/components/NavComponent.vue
@@ -9,23 +9,65 @@
     <b-collapse :visible="visible" is-nav id="nav_collapse">
       <!-- Right aligned nav items -->
       <b-nav vertical class="navbar-nav navbar-sidenav">
-        <b-nav-item @click="hideNavCollapse" to="/users">
+        <b-nav-item v-b-toggle.collapse1 @click="hideNavCollapse" href="#">
           <i class="fa fa-window-maximize"></i>
           
           <b-nav-text>Usuarios</b-nav-text>
+
+          <b-collapse id="collapse1" accordion="my-accordion" class="mt-2">
+            <b-nav-item @click="hideNavCollapse" to="/users">
+              <b-nav-text>Listado</b-nav-text>
+            </b-nav-item>
+            <b-nav-item @click="hideNavCollapse" :to="{ name: 'user_create' }">
+              <b-nav-text>Agregar</b-nav-text>
+            </b-nav-item>
+          </b-collapse>
         </b-nav-item>
 
-        <b-nav-item @click="hideNavCollapse" to="/destinations">
+        <b-nav-item v-b-toggle.collapse2 @click="hideNavCollapse" href="#">
           <i class="fa fa-window-maximize"></i>
           
           <b-nav-text>Destinos</b-nav-text>
+
+          <b-collapse id="collapse2" accordion="my-accordion" class="mt-2">
+            <b-nav-item @click="hideNavCollapse" to="/destinations">
+              <b-nav-text>Listado</b-nav-text>
+            </b-nav-item>
+            <b-nav-item @click="hideNavCollapse" :to="{ name: 'destination_create' }">
+              <b-nav-text>Agregar</b-nav-text>
+            </b-nav-item>
+          </b-collapse>
         </b-nav-item>
 
-        <b-nav-item @click="hideNavCollapse" to="/vouchers">
+        <b-nav-item v-b-toggle.collapse3 @click="hideNavCollapse" href="#">
           <i class="fa fa-window-maximize"></i>
           
           <b-nav-text>Vouchers</b-nav-text>
+
+          <b-collapse id="collapse3" accordion="my-accordion" class="mt-2">
+            <b-nav-item @click="hideNavCollapse" to="/vouchers">
+              <b-nav-text>Listado</b-nav-text>
+            </b-nav-item>
+            <b-nav-item @click="hideNavCollapse" :to="{ name: 'voucher_create' }">
+              <b-nav-text>Agregar</b-nav-text>
+            </b-nav-item>
+          </b-collapse>
         </b-nav-item>
+
+<!--         <b-nav-item-dropdown id="nav7_ddown" text="Usuarios" extra-toggle-classes="nav-link-custom" right>
+          <b-dropdown-item to="/users">Listado</b-dropdown-item>
+          <b-dropdown-item>Agregar</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown id="nav7_ddown" text="Destinos" extra-toggle-classes="nav-link-custom">
+          <b-dropdown-item to="/destinations">Listado</b-dropdown-item>
+          <b-dropdown-item>Agregar</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown id="nav7_ddown" text="Vouchers" extra-toggle-classes="nav-link-custom" right>
+          <b-dropdown-item to="/vouchers">Listado</b-dropdown-item>
+          <b-dropdown-item>Agregar</b-dropdown-item>
+        </b-nav-item-dropdown> -->
       </b-nav>
 
       <b-navbar-nav class="ml-auto">
