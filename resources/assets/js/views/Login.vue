b62d7dd3d2f2af79bb8a2d598816d16b7368960b
Vidal Tomás <vtcheqeeh@gmail.com>
2018-10-29 09:30:36 -0300

    change front login

--- resources/assets/js/views/Login.vue
+++ resources/assets/js/views/Login.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-6 col-md-offset-2">
+                <div class="text-center">
+                    <img src="//www.columbiaviajes.com.ar/img/columbia-logo.png" alt="Columbia Viajes">
+                </div>
                 <form @submit.prevent="login">
                     <div>
                         <div class="form-group">
                             <label for="email">Email</label>
 
-                            <input type="email" class="form-control" id="email" placeholder="Escribe tu email" v-model="user.email">
+                            <input type="email" class="form-control" id="email" placeholder="Escribe tu email" v-model="user.email" required>
                         </div>
 
                         <div class="form-group">
                             <label for="password">Contraseña</label>
 
-                            <input type="password" class="form-control" id="password" placeholder="Escribe tu contraseña" v-model="user.password">
+                            <input type="password" class="form-control" id="password" placeholder="Escribe tu contraseña" v-model="user.password" required>
                         </div>
                     </div>
 
@@ -54,5 +57,11 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+}
+form{
+    padding: 10px;
+    background: #f3f3f3;
+    margin-top: 10px;
+    border-radius: 10px;
 }
 </style>
