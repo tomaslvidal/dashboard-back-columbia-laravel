05d1920076ceb0c8ed5c47ba52a755bfe63a05b3
Vidal Tomás <vtcheqeeh@gmail.com>
2018-11-02 15:54:27 -0300

    scripts with attributes defer

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -28,9 +28,9 @@
 
         <script defer src="https://use.fontawesome.com/releases/v5.2.0/js/all.js" integrity="sha384-4oV5EgaV02iISL2ban6c/RmotsABqE4yZxZLcYMAdG7FAPsyHYAPpywE9PJo+Khy" crossorigin="anonymous"></script>
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script defer src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-        <script src="{{ asset('js/ckeditor/ckeditor.js?v='.filemtime(public_path('js/ckeditor/ckeditor.js'))) }}"></script>
+        <script defer src="{{ asset('js/ckeditor/ckeditor.js?v='.filemtime(public_path('js/ckeditor/ckeditor.js'))) }}"></script>
 
         @yield('script')
     </body>
