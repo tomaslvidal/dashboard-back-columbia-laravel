ffd6e272fbbfc2a866e0ec09c918f2cac0ba7a59
Vidal Tomás <vtcheqeeh@gmail.com>
2018-11-02 15:43:54 -0300

    installing tool laravel-debugbar

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         // Columbia\Providers\BroadcastServiceProvider::class,
         Columbia\Providers\EventServiceProvider::class,
         Columbia\Providers\RouteServiceProvider::class,
-
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
@@ -225,7 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
