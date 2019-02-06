60c011962592f696f553f6cacecd67599766bd1e
Vidal Tomás <vtcheqeeh@gmail.com>
2019-02-06 17:37:13 -0300

    upgrade item voucher tr

--- resources/assets/js/views/vouchers/item.vue
+++ resources/assets/js/views/vouchers/item.vue
@@ -1,5 +1,5 @@
 <template>
-    <tr :key="index">
+    <tr>
         <th scope="row">
             <div class="number d-flex justify-content-start align-items-center">
                 <div class="ball d-flex justify-content-center align-items-center">
@@ -7,34 +7,77 @@
                 </div>
 
                 <div class="d-flex justify-content-end align-items-end" style="padding-left: 10px;">
-                    <i :key="1" style="color: #007bff;" v-if="item.apart.loading==2" class="fas fa-circle-notch fa-spin"></i>
+                    <div class="d-flex justify-content-center align-items-center" style="width: 46px; height: 46px">
+                        <progress-radius v-if="item.apart.loading==2" :radius="23" :progress_item="item.apart.progress" :stroke="4"></progress-radius>
 
-                    <i :key="2" style="color: #48b461;" v-else-if="item.apart.loading==3" class="fas fa-check"></i>
+                        <i :key="2" style="color: #28a745;" v-else-if="item.apart.loading==3" class="far fa-check-circle"></i>
 
-                    <i :key="3" style="color: ##ff0059;" v-else-if="item.apart.loading==4" class="fas fa-times"></i>
+                        <i :key="3" style="color: #a90000;" v-else-if="item.apart.loading==4" class="fas fa-exclamation-circle"></i>
+                    </div>
                 </div>
             </div>
         </th>
 
         <td>{{item.data.get('name')}}</td>
 
-        <td style="">
-        </td>
+        <td>{{item.data.get('description')}}</td>
 
-        <td style="text-align: start;">
-            <!-- <button type="button" class="btn btn-sm btn-primary">Ver</button> -->
-        </td>
+        <!-- <td style="text-align: start;">
+            <button type="button" class="btn btn-sm btn-primary">Ver</button>
+        </td> -->
     </tr>
 </template>
 
 <script>
 export default {
-    props: ['item', 'index'],
+    components: {
+		'progress-radius': require('../../components/utilities/Progress.vue'),
+	},
+    props: ['item', 'index', 'upload_status', 'updateProgress'],
+    data(){
+        return {
+            state: {
+                upload: false
+            }
+        };
+    },
+    watch: {
+        upload_status: function () {
+            this.upload_status==true ? (!this.state.upload ? this.upload() : null) : null;
+        },
+        'item.apart.progress': function (){
+            this.updateProgress();
+        }
+    },
     methods: {
+        upload(){
+            const config = {
+                onUploadProgress: (progressEvent) => {
+                    let percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+
+                    this.item.apart.progress = percentCompleted;
+                }
+            };
+
+            this.state.upload = true;
+
+            this.$set(this.item.apart, 'loading', 2);
+
+            axios.post('/api/vouchers', this.item.data, config)
+            .then((res)=>{
+                this.$set(this.item.apart, 'loading', 3)
+            })
+            .catch(()=>{
+                this.$set(this.item.apart, 'loading', 4)
+            });
+        }
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+    td{
+        vertical-align: middle;
+        text-align: start!important;
+    }
 </style>
